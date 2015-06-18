9c172fc94c87affe1544c77260cfcd92c1897359
Blair Hamilton <bhamilton@draftkings.com>
2015-06-18 14:41:04 -0400

    adding a supports metadata tag

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,6 @@ description       'Installs/Configures nuget'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.0.0'
 
+supports 'windows', '>= 6.1'
+
 depends 'chocolatey'
