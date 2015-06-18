34e9430ce45b6ec85ab9bc31e487ffdc996ebbc7
Blair Hamilton <bhamilton@draftkings.com>
2015-06-18 14:46:41 -0400

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'developers@draftkings.com'
 license           'Apache 2.0'
 description       'Installs/Configures nuget'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.0.0'
+version           '1.0.1'
 
 supports 'windows', '>= 6.1'
 
